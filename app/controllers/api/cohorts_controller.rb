1fa18d0b1fc181d1ca2b6704814d60d3640e7e3f
Shandra <shandradev85@gmail.com>
2019-04-02 13:28:34 -0600

    completed cohort controller

--- app/controllers/api/cohorts_controller.rb
+++ app/controllers/api/cohorts_controller.rb
@@ -1,3 +1,37 @@
 class Api::CohortsController < ApplicationController
-  
+  # Read
+  def index
+    render json: Cohort.all
+  end
+
+  # create
+  def create
+    @cohort = Cohort.new(item_params)
+    if @cohort.save
+      render json: @cohort
+    else
+      render json: { errors: @cohort.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # update
+  def update
+    if @cohort.update(item_params)
+      render json: @cohort
+    else
+      render json: { errors: @cohort.errors }, status: :unprocessable_entity
+    end
+    
+  end
+
+  #delete
+  def destroy
+    Cohort.find(params[:id]).destroy
+    render json:  { message: 'cohort is deleted'}
+  end
+
+  private
+    def item_params
+      params.require(:cohort).permit(:class_name)
+    end
 end
