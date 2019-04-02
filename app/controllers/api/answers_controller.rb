ab356038490954101b0ed2ed5f7a51340105d64f
Du Nguyen <nguyend33@gmail.com>
2019-04-02 15:17:00 -0600

    add answer & post controllers

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,3 +1,35 @@
 class Api::AnswersController < ApplicationController
-  
+  def index
+    render json: Answer.all 
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    if @answer.update(answer_params)
+      render json: @answer
+    else
+      render json: { errors: @answer.errors }
+    end
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      render json: @answer
+    else
+      render json: { errors: @answer.errors }
+    end
+  end
+
+  def destroy
+    Answer.find(params[:id]).destroy
+    render json: { message: 'Answer got deleted' }
+  end
+
+  private 
+    def answer_params
+      params.require(:post).permit(:body)
+    end
+
+end
 end
